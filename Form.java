f3dff8b4829c02e4bebcb70b2eb927f6f04030ce
DikshaMishra03 <mishradiksha483@gmail.com>
2025-11-01 13:08:45 +0530

    Update Form.java

--- Form.java
+++ Form.java
@@ -7,13 +7,16 @@ public class Form {
         frame.setSize(800,800);
         frame.setLayout(new FlowLayout());
         JLabel l1 = new JLabel("Username :");
+        l1.setBounds(20,20,100,50);
         frame.add(l1);
         JTextField text = new JTextField(20);
         frame.add(text);
         JLabel l2 = new JLabel("Password :");
+        l2.setBounds(20,60,100,50);
         frame.add(l2);
         JTextField t2 = new JTextField(8);
         frame.add(t2);
         frame.setVisible(true);
     }
 }
+
